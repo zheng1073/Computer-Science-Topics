1e08d4668d07cb8617657a17220bb5204a130baf
Jason Zheng <jason1996zheng@gmail.com>
2021-10-17 11:27:02 -0400

    Update SquareNumbers.java

--- OOP in Java/Classes and Objects Challenges/SquareNumbers.java
+++ OOP in Java/Classes and Objects Challenges/SquareNumbers.java
@@ -1,3 +1,14 @@
 /*
-Implement a method squareSum(double num1, double num2, double num3) in a class SquareSum which takes three numbers, square them and returns their sum.
+Implement a method squareSum(double num1, double num2, double num3) in a class SquareSum which takes three numbers, 
+square them and returns their sum.
 */
+// Square Sum
+class SquareSum {
+
+  // Method to sum the squares of three numbers
+  double squareSum(double num1, double num2, double num3) {
+    return num1*num1 + num2*num2 + num3*num3;
+  }
+  
+}
+
